27718ff28c56d1ab14cee624ea7103dcdfbbad48
Ludwig <xuezhou@Ludwig.local>
2021-09-26 23:55:23 +0200

    add

--- cat.py
+++ cat.py
@@ -2,8 +2,12 @@ import streamlit as st
 import requests
 from PIL import Image
 import urllib.request
+
 st.title('Random Cats')
 response = requests.get("https://aws.random.cat/meow")
 for i,j in response.json().items():
 	urllib.request.urlretrieve(j, 'cat')
-	st.image(Image.open('cat'))
+	img = Image.open('cat')
+	(width, height) = (img.width//2,img.height//2)
+	st.image(img.resize((width,height), Image.ANTIALIAS))
+
